b162bd42c080c0532bdfe0aa5cff569e27b7d1b0
Mohammed-ELiyaz <122004592+Mohammed-ELiyaz@users.noreply.github.com>
2023-05-29 11:55:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/testthreeinstant/QrCode/QRScannerActivity.java
@@ -0,0 +1,42 @@
+package com.example.testthreeinstant.QrCode;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class QRScannerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Start the QR code scanner
+       IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("Scan a QR code");
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Handle the result of the QR code scanning
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null && result.getContents() != null) {
+            // QR code was successfully scanned
+            String scannedData = result.getContents();
+            Intent intent = new Intent(QRScannerActivity.this, LoginActivity.class);
+            startActivity(intent);
+            // Do something with the scanned data
+        } else {
+            // QR code scanning was canceled or failed
+            finishActivity(requestCode);
+        }
+
+        finish();
+    }
+}
